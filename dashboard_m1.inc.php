4a1e067258a8c6ec930a3f0c3426f4de92f9eb16
umbi <umbi@linkingtechnologies.it>
2024-06-29 13:08:22 +0200

    Update dashboard_m1.inc.php

--- dashboard_m1.inc.php
+++ dashboard_m1.inc.php
@@ -1,6 +1,6 @@
 <?php
 /*  This File is part of Camila PHP Framework
-    Copyright (C) 2006-2022 Umberto Bresciani
+    Copyright (C) 2006-2024 Umberto Bresciani
 
     Camila PHP Framework is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -20,71 +20,50 @@ $camilaWT->db = $_CAMILA['db'];
 
 $rSheet = $camilaWT->getWorktableSheetId('REPORT');
 $aSheet = $camilaWT->getWorktableSheetId('BROGLIACCIO');
+$CSheet = $camilaWT->getWorktableSheetId('COMUNICAZIONI RADIO');
 
 require(CAMILA_LIB_DIR.'/fpdf/fpdf.php');
 
 
-class PDF extends FPDF
-{
+class PDF extends FPDF {
 
-function Header()
-{
-	$t = new CamilaTemplate('it');
-    // Select Arial bold 15
-    $this->SetFont('Arial','B',15);
-    // Move to the right
-    //$this->Cell(80);
-    // Framed title
-    $this->Cell(0,10,'Intervento "' . $t->getParameters()['evento'] . '"',0,0,'C');
-    // Line break
-    $this->Ln(20);
-}
+	function Header()
+	{
+		$t = new CamilaTemplate('it');
+		$this->SetFont('Arial','B',15);
+		$this->Cell(0,10,'Intervento "' . $t->getParameters()['evento'] . '"',0,0,'C');
+		$this->Ln(20);
+	}
 
-function Footer()
-{
-    // Go to 1.5 cm from bottom
-    $this->SetY(-15);
-    // Select Arial italic 8
-    $this->SetFont('Arial','I',8);
-    // Print centered page number
-    $this->Cell(0,10,CAMILA_APPLICATION_NAME . ' - Report del '.date('m/d/Y') . ' ore ' . date('H:i').' - Pagina '.$this->PageNo(),0,0,'C');
-}
+	function Footer()
+	{
+		$this->SetY(-15);
+		$this->SetFont('Arial','I',8);
+		$this->Cell(0,10,CAMILA_APPLICATION_NAME . ' - Report del '.date('m/d/Y') . ' ore ' . date('H:i').' - Pagina '.$this->PageNo(),0,0,'C');
+	}
 
-function ImprovedTable2($obj, $data)
-{
-	$this->Write(10,$obj->title);
-    // Column widths
-    $w = array(90, 30);
-    // Header
-    //for($i=0;$i<count($header);$i++)
-    //    $this->Cell($w[$i],7,$header[$i],1,0,'C');
-    $this->Ln();
-    // Data
-
-	$sum = $obj->sum;
-	$total = 0;
-    foreach($data as $k=>$v)
-    {
-        $this->Cell($w[0],6,$k,1);
-        $this->Cell($w[1],6,$v,1,1);
-        //$this->Cell($w[2],6,number_format($row[2]),'LR',0,'R');
-        //$this->Cell($w[3],6,number_format($row[3]),'LR',0,'R');
-        //$this->Ln();
-		if ($sum != '')
-				{
-					$total += $v;
-				}
-    }
-	
-			if ($total>0) {
-				$this->Cell($w[0],6,'',1);
-        $this->Cell($w[1],6,$total,1,1);
-
+	function ImprovedTable2($obj, $data)
+	{
+		$this->Write(10,$obj->title);
+		$w = array(90, 30);
+		$this->Ln();
+		$sum = $obj->sum;
+		$total = 0;
+		foreach($data as $k=>$v)
+		{
+			$this->Cell($w[0],6,$k,1);
+			$this->Cell($w[1],6,$v,1,1);
+			if ($sum != '')
+			{
+				$total += $v;
 			}
-	
-    // Closing line
-    //$this->Cell(array_sum($w),0,'','T');
-}
+		}
+
+		if ($total>0) {
+			$this->Cell($w[0],6,'',1);
+			$this->Cell($w[1],6,$total,1,1);
+		}	
+	}
 
 }
 
@@ -111,37 +90,10 @@ if (isset($_REQUEST['gid'])) {
 
 if (!isset($_REQUEST['export'])) {
 	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="row">'));
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-4">'));
-	$camilaUI->insertButton('?dashboard=m1&export=pdf', 'Situazione attuale (PDF)', 'file');
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-4">'));
-	$camilaUI->insertButton('cf_worktable'.$aSheet.'.php', 'Brogliaccio attività', 'list');
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-4">'));
-	$camilaUI->insertButton('cf_worktable'.$rSheet.'.php', 'Report operativi', 'list');
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
-
-	$camilaUI->insertDivider();
+	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-9">'));
 	
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="row">'));	
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-4">'));	
-	$camilaUI->insertButton('?dashboard=12', 'Mappa servizi', 'map-marker');
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-4">'));
-	$camilaUI->insertButton('?dashboard=12l', 'Mappa servizi (locale)', 'map-marker');
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-4">'));
-	$camilaUI->insertButton('?dashboard=01', 'Riepilogo ospiti e risorse', 'list-alt');
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
-	
-	$camilaUI->insertDivider();
-
 	$camilaUI->insertTitle('Situazione attuale', 'signal');
-
-	$camilaUI->insertDivider();
-		
+	
 	foreach ($reports as $k => $v) {
 		$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="row">'));	
 		$query = $v->query;
@@ -152,7 +104,7 @@ if (!isset($_REQUEST['export'])) {
 		{
 			$v3 = $arr[$i];
 			if ($v3->type == 'pie' || $v3->type == 'bar') {
-				$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-6">'));
+				$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-8">'));
 				$image1 = new HAW_image("?dashboard=m1&rid=".$v2->id.'&gid='.$v3->id, "?dashboard=m1&rid=".$v->id.'&gid='.$v3->id, ":-)");
 				$image1->set_br(1);
 				if (count($data)>0)
@@ -166,7 +118,7 @@ if (!isset($_REQUEST['export'])) {
 				$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
 			}
 			if ($v3->type == 'table') {
-				$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-6">'));
+				$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-4">'));
 				$myDiv = new HAW_raw(HAW_HTML, $camilaReport->createTable($v3->id, $v3, $data));
 				$_CAMILA['page']->add_raw($myDiv);
 				$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
@@ -176,27 +128,33 @@ if (!isset($_REQUEST['export'])) {
 		
 		$camilaUI->insertDivider();
 	}
+	
+	
+	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
+	
+	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-3">'));
+	$camilaUI->insertTitle('Menu', 'list');
+	$camilaUI->insertButton('?dashboard=m1&export=pdf', 'Situazione attuale (PDF)', 'file');
+	$camilaUI->insertButton('cf_worktable'.$aSheet.'.php', 'Brogliaccio attività', 'list');
+	$camilaUI->insertButton('cf_worktable'.$rSheet.'.php', 'Report operativi', 'list');	
+	$camilaUI->insertButton('cf_worktable'.$cSheet.'.php', 'Comunicazioni radio', 'list');	
+	$camilaUI->insertButton('?dashboard=12', 'Mappa servizi', 'map-marker');
+	$camilaUI->insertButton('?dashboard=12l', 'Mappa servizi (locale)', 'map-marker');
+	$camilaUI->insertButton('?dashboard=01', 'Riepilogo ospiti e risorse', 'list-alt');
+	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
+	
+	$refrCode = "<script>function refreshPage() {window.location.reload();};setInterval(refreshPage, 60000);</script>";
+	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, $refrCode));
+
 }
 else
 {
-		/*ini_set('display_errors', 1);
-		ini_set('display_startup_errors', 1);
-		error_reporting(E_ALL);
-	*/
+	/*ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);*/
 
 	$pdf = new PDF();
-	// Column headings
-	//$header = array('Country', 'Capital', 'Area (sq km)', 'Pop. (thousands)');
-	// Data loading
-	//$data = $pdf->LoadData('countries.txt');
-	$pdf->SetFont('Arial','',10);
-	//$pdf->AddPage();
-	//$pdf->BasicTable($header,$data);
-	//$pdf->AddPage();
-	//$pdf->ImprovedTable($header,$data);
-	//$pdf->AddPage();
-	//$pdf->FancyTable($header,$data);
-	
+	$pdf->SetFont('Arial','',10);	
 	$pageAdded = false;
 
 	foreach ($reports as $k => $v) {
@@ -219,7 +177,6 @@ else
 				}
 			}
 			if ($v3->type == 'table') {
-				//print_r($data);
 				if (count($data)>0) {
 					if (!$pageAdded) {
 						$pdf->AddPage();
