dac937f8f239f0882bc371dad4ca7927d125d1f8
umbi <umbi@linkingtechnologies.it>
2025-03-23 18:54:15 +0100

    Sample data import

--- dashboard_m2.inc.php
+++ dashboard_m2.inc.php
@@ -1,6 +1,6 @@
 <?php
 /*  This File is part of Camila PHP Framework
-    Copyright (C) 2006-2022 Umberto Bresciani
+    Copyright (C) 2006-2025 Umberto Bresciani
 
     Camila PHP Framework is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have rece0ived a copy of the GNU General Public License
     along with Camila PHP Framework. If not, see <http://www.gnu.org/licenses/>. */
 
 $camilaWT  = new CamilaWorkTable();
@@ -20,28 +20,19 @@ $camilaWT->db = $_CAMILA['db'];
 
 $sSheet = $camilaWT->getWorktableSheetId('SERVIZI');
 
-//$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="row">'));
-//$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-12">'));
 $camilaUI->insertTitle('Evento', 'cog');
-//$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div></div>'));
 	
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="row">'));
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-4">'));
-
 $camilaUI->insertButton('?dashboard=05', 'Informazioni intervento','calendar');
-
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-4">'));
-
 $camilaUI->insertButton('cf_worktable'.$sSheet.'.php', 'Elenco servizi', 'list');
-
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-4">'));
-
 $camilaUI->insertButton('?dashboard=06', 'Carica loghi','upload');
-
+$camilaUI->insertButton('?dashboard=iw', 'Dati esempio','upload');
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
-
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div><br/>'));
 
 
@@ -49,9 +40,7 @@ $camilaUI->insertTitle('Server', 'hdd');
 
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="row">'));
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-4">'));
-
 $camilaUI->insertButton('?dashboard=00', camila_get_translation('camila.serveraddress'),'globe');
-
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-4">'));
 $camilaUI->insertButton('?dashboard=30', 'Backup dati','download');
@@ -59,7 +48,6 @@ $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-4">'));
 $camilaUI->insertButton('?dashboard=31', 'Backup intero database','download');
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
-
 $_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
 
 
