0d322fd8a7d70ff1607e0a08ca850a01f41f8175
umbi <umbi@linkingtechnologies.it>
2025-04-23 23:20:03 +0200

    Aggiornamento reportistica

--- dashboard_m1.inc.php
+++ dashboard_m1.inc.php
@@ -31,22 +31,25 @@ if (isset($_REQUEST['gid'])) {
 
 if (!isset($_REQUEST['format'])) {
 	$camilaUI->openBox();
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="row">'));
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div class="row">'));
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="row">'));
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-9">'));
+	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="row columns">'));
+	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div class="row columns">'));
+	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="row columns">'));
+	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-9 column is-12-mobile is-9-desktop">'));
 	$camilaUI->insertTitle($camilaReport->getCurrentReportTitle(), 'dashboard');
 	$camilaReport->outputHtmlToBrowser();
 	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '</div>'));
-	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-3">'));
+	$_CAMILA['page']->add_raw(new HAW_raw(HAW_HTML, '<div class="col-xs-12 col-md-3 column is-12-mobile is-3-desktop">'));
 	$camilaUI->insertTitle('Report', 'dashboard');
 	foreach ($camilaReport->getReports() as $k => $v) {
 		if ($k == $camilaReport->getCurrentReportName()) {
 			$camilaUI->insertButton('?dashboard=m1&report='.$_GET['report'], $camilaReport->getCurrentReportTitle(), 'dashboard');
 			$camilaUI->insertSecondaryButton('?dashboard=m1&format=pdf&report='.$_GET['report'], $camilaReport->getCurrentReportTitle().' (in PDF)', 'file');
+			$camilaUI->insertSecondaryButton('?dashboard=m1&format=docx&report='.$_GET['report'], $camilaReport->getCurrentReportTitle().' (in .docx)', 'file');
 		} else {
 			$camilaUI->insertSecondaryButton('?dashboard=m1&report='.$k, $v, 'dashboard');
 			$camilaUI->insertSecondaryButton('?dashboard=m1&format=pdf&report='.$k, $v. ' (in PDF)', 'file');
+			$camilaUI->insertSecondaryButton('?dashboard=m1&report='.$k, $v, 'dashboard');
+			$camilaUI->insertSecondaryButton('?dashboard=m1&format=docx&report='.$k, $v. ' (in .docx)', 'file');
 		}
 	}
 	$camilaUI->insertTitle('Altre risorse', 'list');
@@ -60,10 +63,10 @@ if (!isset($_REQUEST['format'])) {
 
 if (isset($_GET['format']) && $_GET['format'] == 'pdf') {
 	$camilaReport->outputPdfToBrowser();
-} elseif (isset($_GET['format']) && $_GET['format'] == 'word') {
-	$camilaReport->outputWordToBrowser();
-} elseif (isset($_GET['format']) && $_GET['format'] == 'odf') {
-	$camilaReport->outputOdfToBrowser();
+} elseif (isset($_GET['format']) && $_GET['format'] == 'docx') {
+	$camilaReport->outputDocxToBrowser();
+} elseif (isset($_GET['format']) && $_GET['format'] == 'odt') {
+	$camilaReport->outputOdtToBrowser();
 }
 
 ?>
