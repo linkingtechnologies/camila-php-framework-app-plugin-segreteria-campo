cd3ac214060daad39951cce0a0fdaaefe779e076
umbi <umbi@linkingtechnologies.it>
2025-10-12 14:22:26 +0200

    Aggiornamento sezione preaccreditamente

--- pre-accreditations.inc.php
+++ pre-accreditations.inc.php
@@ -1,118 +1,137 @@
 <?php
-$camilaWT  = new CamilaWorkTable();
-$camilaWT->db = $_CAMILA['db'];
+if (!isset($_REQUEST['camila_xml2pdf'])) {
+	$camilaWT  = new CamilaWorkTable();
+	$camilaWT->db = $_CAMILA['db'];
 
-$r1Sheet = $camilaWT->getWorktableSheetId('VOLONTARI PREACCREDITATI');
-$r2Sheet = $camilaWT->getWorktableSheetId('MEZZI PREACCREDITATI');
+	$r1Sheet = $camilaWT->getWorktableSheetId('VOLONTARI PREACCREDITATI');
+	$r2Sheet = $camilaWT->getWorktableSheetId('MEZZI PREACCREDITATI');
 
-$camilaWT = new CamilaWorkTable();
-$camilaWT->db = $_CAMILA['db'];
+	$camilaWT = new CamilaWorkTable();
+	$camilaWT->db = $_CAMILA['db'];
 
-$sql = "SELECT DISTINCT \${VOLONTARI PREACCREDITATI.TURNO} FROM \${VOLONTARI PREACCREDITATI} ORDER BY \${VOLONTARI PREACCREDITATI.TURNO}";
-$r = $camilaWT->queryWorktableDatabase($sql);
+	$sql = "SELECT DISTINCT \${VOLONTARI PREACCREDITATI.TURNO} FROM \${VOLONTARI PREACCREDITATI} ORDER BY \${VOLONTARI PREACCREDITATI.TURNO}";
+	$r = $camilaWT->queryWorktableDatabase($sql);
 
-$camilaUI->openBox();
+	$camilaUI->openBox();
 
-$turniCsv = implode(',',array_keys($r));
+	$turniCsv = implode(',',array_keys($r));
 
-$camilaUI->openMenuSection('Preaccreditamenti');
+	$camilaUI->openMenuSection('Preaccreditamenti');
 
-$camilaUI->addItemToMenuSection('?dashboard=pre-accreditations-by-service','Volontari');
-$camilaUI->addItemToMenuSection('?dashboard=pre-accreditations-vehicles-by-service','Mezzi');
-$camilaUI->closeMenuSection();
+	$camilaUI->addItemToMenuSection('?dashboard=pre-accreditations-by-service','Volontari');
+	$camilaUI->addItemToMenuSection('?dashboard=pre-accreditations-vehicles-by-service','Mezzi');
+	$camilaUI->closeMenuSection();
 
-$camilaUI->insertDivider();
+	$camilaUI->insertDivider();
 
-if ($_REQUEST['dashboard'] == 'pre-accreditations-by-service') {
-	$mapping = '';
-	$title = 'Volontari preaccreditati per turno ' . $_REQUEST['custom'];
-	$camilaUI->insertTitle($title, 'user');
-	$camilaUI->insertSecondaryButton('?dashboard=pre-accreditations-by-service', 'Riepilogo per turno', 'tools', false);
-	foreach($r as $k => $v) {
-		$camilaUI->insertSecondaryButton('?dashboard=pre-accreditations-by-service&custom='.urlencode($k), $k, 'calendar', false);
+	if ($_REQUEST['dashboard'] == 'pre-accreditations-by-service') {
+		$mapping = '';
+		$title = 'Volontari preaccreditati per turno ' . $_REQUEST['custom'];
+		$camilaUI->insertTitle($title, 'user');
+		$camilaUI->insertSecondaryButton('?dashboard=pre-accreditations-by-service', 'Riepilogo per turno', 'tools', false);
+		foreach($r as $k => $v) {
+			if ($k == $_REQUEST['custom'])
+				$camilaUI->insertButton('?dashboard=pre-accreditations-by-service&custom='.urlencode($k), $k, 'calendar', false);
+			else
+				$camilaUI->insertSecondaryButton('?dashboard=pre-accreditations-by-service&custom='.urlencode($k), $k, 'calendar', false);
+		}
+		$camilaUI->insertLineBreak();
+		$stmt = build_pivot_sql('SERVIZIO',$turniCsv);
+		$stmt = $camilaWT->parseWorktableSqlStatement($stmt);
+		$report = new report($stmt, '', 'organizzazione', '', $mapping);
+		$report->canupdate = false;
+		$report->candelete = false;
+		$report->drawfilterbox = false;
+		$report->drawapplytemplate = false;
+		$report->process();
+		$report->draw();
+	} else if ($_REQUEST['dashboard'] == 'pre-accreditations-by-overnight-stay') {
+		$mapping = '';
+		$title = 'Pernottamenti per turno ' . $_REQUEST['custom'];
+		$camilaUI->insertTitle($title, 'user');
+		$camilaUI->insertSecondaryButton('?dashboard=pre-accreditations-by-overnight-stay', 'Riepilogo per turno', 'tools', false);
+		foreach($r as $k => $v) {
+			$camilaUI->insertSecondaryButton('?dashboard=pre-accreditations-by-overnight-stay&custom='.urlencode($k), $k, 'calendar', false);
+		}
+		$camilaUI->insertLineBreak();
+		$stmt = build_pivot_sql('PERNOTTAMENTO',$turniCsv);
+		$stmt = $camilaWT->parseWorktableSqlStatement($stmt);
+		$report = new report($stmt, '', 'organizzazione', '', $mapping);
+		$report->canupdate = false;
+		$report->candelete = false;
+		$report->drawfilterbox = false;
+		$report->drawapplytemplate = false;
+		$report->process();
+		$report->draw();
+	} else if ($_REQUEST['dashboard'] == 'pre-accreditations-vehicles-by-service') {
+		$mapping = '';
+		$title = 'Mezzi preaccreditati per turno ' . $_REQUEST['custom'];
+		$camilaUI->insertTitle($title, 'truck');
+		$camilaUI->insertSecondaryButton('?dashboard=pre-accreditations-vehicles-by-service', 'Riepilogo per turno', 'tools', false);
+		foreach($r as $k => $v) {
+			$camilaUI->insertSecondaryButton('?dashboard=pre-accreditations-vehicles-by-service&custom='.urlencode($k), $k, 'calendar', false);
+		}
+		$camilaUI->insertLineBreak();
+		$stmt = build_pivot_sql_2('SERVIZIO',$turniCsv);
+		$stmt = $camilaWT->parseWorktableSqlStatement($stmt);
+		$report = new report($stmt, '', 'organizzazione', '', $mapping);
+		$report->canupdate = false;
+		$report->candelete = false;
+		$report->drawfilterbox = false;
+		$report->drawapplytemplate = false;
+		$report->process();
+		$report->draw();
 	}
-	$camilaUI->insertLineBreak();
-	$stmt = build_pivot_sql('SERVIZIO',$turniCsv);
-	$stmt = $camilaWT->parseWorktableSqlStatement($stmt);
-	$report = new report($stmt, '', 'organizzazione', '', $mapping);
-	$report->canupdate = false;
-	$report->candelete = false;
-	$report->drawfilterbox = false;
-	$report->drawapplytemplate = false;
-	$report->process();
-	$report->draw();
-} else if ($_REQUEST['dashboard'] == 'pre-accreditations-by-overnight-stay') {
-	$mapping = '';
-	$title = 'Pernottamenti per turno ' . $_REQUEST['custom'];
-	$camilaUI->insertTitle($title, 'user');
-	$camilaUI->insertSecondaryButton('?dashboard=pre-accreditations-by-overnight-stay', 'Riepilogo per turno', 'tools', false);
-	foreach($r as $k => $v) {
-		$camilaUI->insertSecondaryButton('?dashboard=pre-accreditations-by-overnight-stay&custom='.urlencode($k), $k, 'calendar', false);
-	}
-	$camilaUI->insertLineBreak();
-	$stmt = build_pivot_sql('PERNOTTAMENTO',$turniCsv);
-	$stmt = $camilaWT->parseWorktableSqlStatement($stmt);
-	$report = new report($stmt, '', 'organizzazione', '', $mapping);
-	$report->canupdate = false;
-	$report->candelete = false;
-	$report->drawfilterbox = false;
-	$report->drawapplytemplate = false;
-	$report->process();
-	$report->draw();
-} else if ($_REQUEST['dashboard'] == 'pre-accreditations-vehicles-by-service') {
-	$mapping = '';
-	$title = 'Mezzi preaccreditati per turno ' . $_REQUEST['custom'];
-	$camilaUI->insertTitle($title, 'truck');
-	$camilaUI->insertSecondaryButton('?dashboard=pre-accreditations-vehicles-by-service', 'Riepilogo per turno', 'tools', false);
-	foreach($r as $k => $v) {
-		$camilaUI->insertSecondaryButton('?dashboard=pre-accreditations-vehicles-by-service&custom='.urlencode($k), $k, 'calendar', false);
-	}
-	$camilaUI->insertLineBreak();
-	$stmt = build_pivot_sql_2('SERVIZIO',$turniCsv);
-	$stmt = $camilaWT->parseWorktableSqlStatement($stmt);
-	$report = new report($stmt, '', 'organizzazione', '', $mapping);
-	$report->canupdate = false;
-	$report->candelete = false;
-	$report->drawfilterbox = false;
-	$report->drawapplytemplate = false;
-	$report->process();
-	$report->draw();
+
+	$camilaUI->addGridSection(2, function ($colIndex) use ($camilaUI, $r, $r1Sheet, $r2Sheet) {
+		switch ($colIndex) {
+			case 0:
+				$camilaUI->insertTitle('Modulistica', 'article');
+				$camilaUI->insertButton('?camila_xml2pdf', 'Elenco complessivo', 'moon');
+				foreach($r as $k => $v) {					
+					$u = '?';
+					$u.= 'camila_worktable_add_child_filter_1=' . urlencode(' AND ${VOLONTARI PREACCREDITATI.TURNO} = \''.$k.'\'');
+					$u.= '&camila_worktable_add_child_filter_2=' . urlencode(' AND ${MEZZI PREACCREDITATI.TURNO} = \''.$k.'\'');
+					$u.= '&camila_worktable_add_child_filter_3=' . urlencode(' AND ${MATERIALI PREACCREDITATI.TURNO} = \''.$k.'\'');
+					$u.= '&camila_xml2pdf';
+					$camilaUI->insertButton($u, 'Turno ' . $k, 'article');
+				}
+				break;
+
+			case 1:
+				$camilaUI->insertTitle('Pernottamenti', 'moon');
+				$camilaUI->insertButton('?dashboard=pre-accreditations-by-overnight-stay', 'Riepilogo per turno', 'moon');
+				foreach($r as $k => $v) {
+					$camilaUI->insertButton('?dashboard=pre-accreditations-by-overnight-stay&custom='.urlencode($k), $k, 'calendar');
+				}
+				break;
+		}
+	});
+
+	$camilaUI->addGridSection(2, function ($colIndex) use ($camilaUI, $r, $r1Sheet, $r2Sheet) {
+		switch ($colIndex) {
+			case 0:
+				$camilaUI->insertTitle('Elenchi', 'user');
+				$camilaUI->insertButton('cf_worktable'.$r1Sheet.'.php','Elenco complessivo', 'user');
+				foreach($r as $k => $v) {
+					$camilaUI->insertButton('cf_worktable'.$r1Sheet.'.php?camila_w1f=_C_turno&camila_w1c=eq&camila_w1v='.urlencode($k),$k, 'calendar');
+				}
+				break;
+			case 1:
+				$camilaUI->insertTitle('Elenchi', 'truck');
+				$camilaUI->insertButton('cf_worktable'.$r2Sheet.'.php','Elenco complessivo', 'truck');
+				foreach($r as $k => $v) {
+					$camilaUI->insertButton('cf_worktable'.$r2Sheet.'.php?camila_w1f=_C_turno&camila_w1c=eq&camila_w1v='.urlencode($k),$k, 'calendar');
+				}
+				break;
+		}
+	});
+
+	$_CAMILA['page']->camila_export_enabled = true;
+
+	$camilaUI->closeBox();
+
 }
-
-
-
-$camilaUI->addGridSection(3, function ($colIndex) use ($camilaUI, $r, $r1Sheet, $r2Sheet) {
-	switch ($colIndex) {
-		case 0:
-			$camilaUI->insertTitle('Pernottamenti', 'moon');
-			$camilaUI->insertButton('?dashboard=pre-accreditations-by-overnight-stay', 'Riepilogo per turno', 'moon');
-			foreach($r as $k => $v) {
-				$camilaUI->insertButton('?dashboard=pre-accreditations-by-overnight-stay&custom='.urlencode($k), $k, 'calendar');
-			}
-			$camilaUI->insertLineBreak();
-			break;
-		case 1:
-			$camilaUI->insertTitle('Elenchi', 'user');
-			$camilaUI->insertButton('cf_worktable'.$r1Sheet.'.php','Elenco', 'user');
-			foreach($r as $k => $v) {
-				$camilaUI->insertButton('cf_worktable'.$r1Sheet.'.php?camila_w1f=_C_turno&camila_w1c=eq&camila_w1v='.urlencode($k),$k, 'calendar');
-			}
-			break;
-		case 2:
-			$camilaUI->insertTitle('Elenchi', 'truck');
-			$camilaUI->insertButton('cf_worktable'.$r2Sheet.'.php','Elenco', 'truck');
-			foreach($r as $k => $v) {
-				$camilaUI->insertButton('cf_worktable'.$r2Sheet.'.php?camila_w1f=_C_turno&camila_w1c=eq&camila_w1v='.urlencode($k),$k, 'calendar');
-			}
-			break;
-	}
-});
-
-$_CAMILA['page']->camila_export_enabled = true;
-
-$camilaUI->closeBox();
-
-
 
 /** Make a safe backticked alias from the raw shift label */
 function backtick_alias(string $label): string {
